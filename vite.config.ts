7aef59b53033ce4ced0f2fb13905b65d6c36cb9a
satishdonerao <doneraosatish@gmail.com>
2025-03-17 17:32:22 +0530

    env chnages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env, // Ensures that environment variables are correctly injected
+    'process.env': process.env, // This ensures environment variables are loaded properly.
   },
+  envPrefix: 'VITE_', // Make sure only variables prefixed with 'VITE_' are exposed to your code
 });
