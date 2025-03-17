1357a14d8c347e80fa5bc1a77382ffad118014fe
satishdonerao <doneraosatish@gmail.com>
2025-03-17 17:54:13 +0530

    env chnages

--- utils/ApiKey.ts
+++ utils/ApiKey.ts
@@ -1,9 +1,9 @@
 export const BACKEND_API_KEY ="https://packarma.shellcode.cloud"
   import.meta.env["VITE_ENV"] === "DEV"
-    ? "http://localhost:5000/api/admin"
+    ? "https://packarma.shellcode.cloud"
     : import.meta.env["VITE_ADMIN_BACKEND_LINK"];
 
 export const BACKEND_MEDIA_LINK ="https://packarma.shellcode.cloud"
   import.meta.env["VITE_ENV"] === "DEV"
-    ? "http://localhost:5000"
+    ? "https://packarma.shellcode.cloud"
     : import.meta.env["VITE_ADMIN_BACKEND_LINK"];
