7ae6182081b876850664e17604157ed12f37556f
satishdonerao <doneraosatish@gmail.com>
2025-03-17 18:52:38 +0530

    redirect page

--- src/Redirect.tsx
+++ src/Redirect.tsx
@@ -10,7 +10,7 @@ const Redirect = () => {
   }, []);
 
   const handleRedirect = () => {
-    window.location.href = "https://packarma-admin-wa99.vercel.app/admin/login";
+    window.location.href = "https://packarma-admin-wa99.vercel.app";
   };
 
   return (
